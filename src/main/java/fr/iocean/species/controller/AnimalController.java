8a7540081b8f8fb3ca8bb59c2f910a5c8423f39f
atyzeph <bennetot-clm@hotmail.com>
2022-11-10 13:27:10 +0100

    Ajout CRUD animal

--- src/main/java/fr/iocean/species/controller/AnimalController.java
+++ src/main/java/fr/iocean/species/controller/AnimalController.java
@@ -1,6 +1,5 @@
 package fr.iocean.species.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.iocean.species.model.Animal;
 import fr.iocean.species.repository.AnimalRepository;
+import org.springframework.data.domain.Sort;
 
 
 @RequestMapping
@@ -23,10 +24,12 @@ public class AnimalController {
 	
 	@GetMapping("/animals")
 	public String listAnimals(Model model) {
-		List<Animal> animals = (List<Animal>) animalRepository.findAll();
-		model.addAttribute("animals", animals);
+		model.addAttribute(
+				"animals", 
+				animalRepository.findAll(Sort.by(Sort.Direction.ASC, "species"))
+				);
 		
-		return "animals";
+		return "animalsView/animals";
 	}
 	
 	@GetMapping("/animals/{id}")
@@ -35,8 +38,32 @@ public class AnimalController {
 		if (animals.isPresent()) {
 			model.addAttribute(animals.get());
 			
-			return "animals";
+			return "animalsView/animalId";
 		}
 		return "error";
+	}
+	
+	@GetMapping("/animals/create")
+	public String Create(Model model) {
+		
+		model.addAttribute(new Animal());
+		
+		return "animalsView/animalCreate";
+	}
+		
+	@PostMapping("/animals")
+	public String CreateOrUpdate(Animal animal) {
+		
+		this.animalRepository.save(animal);
+		
+		return "redirect:/animals";
+	}
+
+	@GetMapping("/animals/delete/{id}")
+	public String delete(@PathVariable("id") Integer animalId) {
+		Optional<Animal> animalToDelete = this.animalRepository.findById(animalId);
+		animalToDelete.ifPresent(a -> this.animalRepository.delete(a));
+		
+		return "redirect:/animals";
 	}
 }
--- src/main/java/fr/iocean/species/repository/AnimalRepository.java
+++ src/main/java/fr/iocean/species/repository/AnimalRepository.java
@@ -2,9 +2,8 @@ package fr.iocean.species.repository;
 
 import fr.iocean.species.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnimalRepository extends CrudRepository<Animal, Integer> {
+public interface AnimalRepository extends JpaRepository<Animal, Integer> {
 }
