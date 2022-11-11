4f1ee9073c20e926ca11f413844a14229434d2ab
atyzeph <bennetot-clm@hotmail.com>
2022-11-11 11:04:03 +0100

    commit

--- src/main/java/fr/iocean/species/service/DetailsService.java
+++ src/main/java/fr/iocean/species/service/DetailsService.java
@@ -6,21 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import fr.iocean.species.model.User;
 import fr.iocean.species.repository.UserRepository;
 
-public class DetailsDervice implements UserDetailsService {
+@Service
+public class DetailsService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Override
-	@Transactional
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
-		User user = userRepository.findByUsername(username);
-		return (UserDetails) user;
+		return null;
 	}
+	
+//	@Override
+//	@Transactional
+//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		// TODO Auto-generated method stub
+//		User user = userRepository.findByUsername(username);
+//		return (UserDetails) user;
+//	}
 
 }
